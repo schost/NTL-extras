b8fa00007df0159202d34648f93e80cd0c1281c2
Kevin Hyun <ke456@hotmail.com>
2016-10-01 22:44:24 -0400

    hermite pade working in small inputs

--- hermite_pade/hermite_pade.h
+++ hermite_pade/hermite_pade.h
@@ -13,7 +13,7 @@
 
 class hermite_pade{
   Vec<BivariateModularComp> vec_M;
-  cauchy_like_geometric_special invM; //the inverse mod p
+  cauchy_like_geometric_special CL,invA; //the inverse mod p
   Vec<ZZ_p> e, f; // for precondition the Cauchy Matrix
   ZZ c, d; // constant for the preconditioners of M
   Vec<ZZ_pX_Multipoint_FFT> vec_X_int, vec_Y_int; // to store already calculated powers
@@ -34,13 +34,36 @@ class hermite_pade{
   // calculates the order of w
   long find_order(zz_p w);
 
+  // checks if every entry can be reconstructed in the current field
+  bool can_reconstruct(const Vec<ZZ_p> &v, long n);
+
+  // computes (D_e X_int) M (D_f Y_int)^t * b
+  void mulA_right (Vec<ZZ_p> &x, Vec<ZZ_p> b);
+
+  // computes x = Diagonal_d * a where Diagonal_d is the diagonal matrix constructed from d
+  void mul_diagonal_right (Vec<ZZ_p> &x, const Vec<ZZ_p> &d, const Vec<ZZ_p> &a);
+  
+  // returns each block of v (based on the type) flipped
+  Vec<ZZ_p> flip_on_type (const Vec<ZZ_p> &v);
+  
+  void find_original_sol(Vec<ZZ_p> &x, const Vec<ZZ_p> &b);
+  
+  // solves for Mx = b mod p^2^n
+  void DAC (Vec<ZZ_p> &x, const Vec<ZZ_p> &b, long n);
+  
+  void reconstruct(Vec<Vec<ZZ>> &sol, const Vec<ZZ_p> &v, long n);
+  
+  Vec<Vec<ZZ>> flip_on_type(const Vec<Vec<ZZ>> &v);
+
   public:
   /** Ctor ***********************************************
   * f: the generating series                             *
   * type: the type of the approximant                    *
   *******************************************************/
-  hermite_pade(const ZZX &f, const Vec<long>& type, long prec);
-
+  hermite_pade(const ZZX &f, const Vec<long>& type, long prec, long fft_init = 0);
+  
+  void find_rand_sol (Vec<Vec<ZZ>> &sol); 
+ 
   // computes a random solution to the system
   void get_rand_sol(Vec<ZZ> &);
 };
