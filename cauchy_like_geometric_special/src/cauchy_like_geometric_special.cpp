5a77b75ebfd9012cedeee889d1d30be56b826027
eric.schost@gmail.com <schost@yoga>
2016-10-04 01:32:36 -0400

    fixed bug with non GRP matrices

--- cauchy_like_geometric_special/src/cauchy_like_geometric_special.cpp
+++ cauchy_like_geometric_special/src/cauchy_like_geometric_special.cpp
@@ -47,6 +47,165 @@ void to_dense(Mat<zz_p> & A, const cauchy_like_geometric_special& CL){
   }
 }
 
+// /*------------------------------------------------------------*/
+// /* inverts a special-Cauchy-like matrix                       */
+// /* assumes generic rank profile                               */
+// /* the generators are Yp, Zp                                  */
+// /* alpha = displacement rank                                  */
+// /* overwrites Yp, Zp                                          */
+// /* i0, j0 are the initial indices in i, j resp.               */
+// /* i1, j1 are the end indices in i, j resp.                   */  
+// /*------------------------------------------------------------*/
+// static long invert_raw(long *Yp, long *Zp, const long alpha,
+// 		       const cauchy_geometric_special& C,
+// 		       const long i0, const long i1, const long j0, const long j1){
+
+
+//   // P[i+i0]*Qab[j+j0-i-i0+n-1] = 1/(a*power(q,i+i0)-b*power(q,j+j0))
+//   // P[i+i0]*Qa[j+j0-i-i0+n-1] = 1/(a*power(q,i+i0)-a*power(q,j+j0)) = 1/(x[i]-x[j])
+//   // P[i+i0]*Qb[j+j0-i-i0+n-1] = 1/(b*power(q,i+i0)-b*power(q,j+j0)) = 1/(y[i]-y[j])
+
+//   const long n = C.n;
+//   const long p = zz_p::modulus();
+//   mulmod_precon_t *Ypre = new mulmod_precon_t[alpha];
+//   mulmod_precon_t *Zpre = new mulmod_precon_t[alpha];
+//   const mulmod_t pinv = zz_p::ModulusInverse();
+
+//   long *tmpYk = Yp;
+//   long *tmpZk = Zp;
+
+//   cout << "enter with: " << i0 << " " << i1 << " " << j0 << " " << j1 << endl;
+//   long N = min(i1-i0, j1-j0);
+
+//   for (long k = 0; k < N; k++){
+//     long *tmpYi = Yp;
+//     long *tmpZi = Zp;
+
+//     for (long j = 0; j < alpha; j++){
+//       Ypre[j] = PrepMulModPrecon(tmpYk[j], p, pinv);
+//       Zpre[j] = PrepMulModPrecon(tmpZk[j], p, pinv);
+//     }
+
+//     // computes the top-left entry in the current submatrix
+//     long rd = 0;
+//     for (long j = 0; j < alpha; j++)
+//       rd = AddMod(rd, MulMod(tmpZk[j], tmpYk[j], p, pinv), p);
+
+
+//     // early exit if rank(M) < n
+//     if (rd == 0){
+
+//       cout << "with zero pivot with k=" << k<< endl;
+
+//       long *tmpY = tmpYk;
+//       for (long a = k; a < i1-i0; a++){
+//     	long *tmpZ = tmpZk;
+//     	for (long b = k; b < j1-j0; b++){
+//     	  long tmp = 0;
+//     	  for (long j = 0; j < alpha; j++)
+//     	    tmp = AddMod(tmp, MulMod(tmpZ[j], tmpY[j], p, pinv), p);
+//     	  if (tmp != 0) {
+// 	    cout << "failed " << a << " " << b << endl;
+//     	    delete[] Ypre;
+//     	    delete[] Zpre;
+//     	    return -1;  // not generic rank profile
+//     	  }
+// 	  cout << "passed " << a << " " << b << endl;
+//     	  tmpZ += alpha;
+//     	}
+//     	tmpY += alpha;
+//       }
+//       delete[] Ypre;
+//       delete[] Zpre;
+//       return k; // OK, rank = k
+//     }
+
+//     rd = MulModPrecon(rd, C.P_r[k+i0], p, C.P_pre[k+i0]);
+//     rd = MulModPrecon(rd, C.Qab_r[j0-i0+n-1], p, C.Qab_pre[j0-i0+n-1]);
+
+//     zz_p d(rd, INIT_LOOP_HOLE);
+//     zz_p id = 1/d;
+//     zz_p mid = -id;
+
+//     const long rid = rep(id);
+//     const long rmid = rep(mid);
+//     mulmod_precon_t pd = PrepMulModPrecon(rd, p, pinv);
+//     mulmod_precon_t prid = PrepMulModPrecon(rid, p, pinv);
+
+//     for (long i = 0; i < k; i++){
+
+//       long ell = 0, u = 0;
+//       for (long j = 0; j < alpha; j++){
+//       	ell = AddMod(ell, MulModPrecon(tmpYi[j], tmpZk[j], p, Zpre[j]), p);
+//       	u = AddMod(u, MulModPrecon(tmpZi[j], tmpYk[j], p, Ypre[j]), p);
+//       }
+//       // not so great for small alpha
+//       // ell = InnerProd_LL(tmpYi, tmpZk, alpha);   
+//       // u = InnerProd_LL(tmpZi, tmpYk, alpha);
+
+//       ell = MulModPrecon(ell, rid, p, prid);
+//       ell = MulModPrecon(ell, C.P_r[i+i0], p, C.P_pre[i+i0]);
+//       ell = MulModPrecon(ell, C.Qb_r[k+j0-i-i0+n-1], p, C.Qb_pre[k+j0-i-i0+n-1]);
+
+//       u = MulModPrecon(u, rid, p, prid);
+//       u = MulModPrecon(u, C.P_r[i+i0], p, C.P_pre[i+i0]);
+//       u = MulModPrecon(u, C.Qa_r[k+j0-i-i0+n-1], p, C.Qa_pre[k+j0-i-i0+n-1]);
+
+//       for (long j = 0; j < alpha; j++){
+//       	tmpYi[j] = SubMod(tmpYi[j], MulModPrecon(ell, tmpYk[j], p, Ypre[j]), p);
+//       	tmpZi[j] = AddMod(tmpZi[j], MulModPrecon(u, tmpZk[j], p, Zpre[j]), p);
+//       }
+
+//       tmpYi += alpha;
+//       tmpZi += alpha;
+//     }
+
+//     for (long j = 0; j < alpha; j++){
+//       tmpYk[j] = MulModPrecon(rmid, tmpYk[j], p, Ypre[j]);
+//       Ypre[j] = PrepMulModPrecon(tmpYk[j], p, pinv);
+//       tmpZk[j] = MulModPrecon(rid, tmpZk[j], p, Zpre[j]);
+//       Zpre[j] = PrepMulModPrecon(tmpZk[j], p, pinv);
+//     }
+
+//     tmpYi += alpha;
+//     tmpZi += alpha;
+
+//     for (long i = k+1; i < N; i++){
+
+//       long ell = 0, u = 0;
+//       for (long j = 0; j < alpha; j++){
+// 	ell = AddMod(ell, MulModPrecon(tmpYi[j], tmpZk[j], p, Zpre[j]), p);
+//         u = AddMod(u, MulModPrecon(tmpZi[j], tmpYk[j], p, Ypre[j]), p);
+//       }
+
+
+
+//       ell = MulModPrecon(ell, rd, p, pd);  // why ell != u?
+//       ell = MulModPrecon(ell, C.P_r[i+i0], p, C.P_pre[i+i0]);
+//       ell = MulModPrecon(ell, C.Qab_r[k+j0-i-i0+n-1], p, C.Qab_pre[k+j0-i-i0+n-1]);
+
+//       u = MulModPrecon(u, rd, p, pd); // save one product here ?
+//       u = MulModPrecon(u, C.P_r[k+i0], p, C.P_pre[k+i0]);
+//       u = MulModPrecon(u, C.Qab_r[i+j0-k-i0+n-1], p, C.Qab_pre[i+j0-k-i0+n-1]);
+
+//       for (long j = 0; j < alpha; j++){
+//       	tmpYi[j] = AddMod(tmpYi[j], MulModPrecon(ell, tmpYk[j], p, Ypre[j]), p);
+//       	tmpZi[j] = AddMod(tmpZi[j], MulModPrecon(u, tmpZk[j], p, Zpre[j]), p);
+//       }
+
+//       tmpYi += alpha;
+//       tmpZi += alpha;
+//     }
+
+//     tmpYk += alpha;
+//     tmpZk += alpha;
+//   }
+
+//   delete[] Ypre;
+//   delete[] Zpre;
+//   return N;
+// }
+
 /*------------------------------------------------------------*/
 /* inverts a special-Cauchy-like matrix                       */
 /* assumes generic rank profile                               */
@@ -74,6 +233,7 @@ static long invert_raw(long *Yp, long *Zp, const long alpha,
   long *tmpYk = Yp;
   long *tmpZk = Zp;
 
+  //  cout << "enter with: " << i0 << " " << i1 << " " << j0 << " " << j1 << endl;
   long N = min(i1-i0, j1-j0);
 
   for (long k = 0; k < N; k++){
@@ -93,6 +253,9 @@ static long invert_raw(long *Yp, long *Zp, const long alpha,
 
     // early exit if rank(M) < n
     if (rd == 0){
+
+      //      cout << "with zero pivot with k=" << k<< endl;
+
       long *tmpY = tmpYk;
       for (long a = k; a < i1-i0; a++){
     	long *tmpZ = tmpZk;
@@ -101,10 +264,12 @@ static long invert_raw(long *Yp, long *Zp, const long alpha,
     	  for (long j = 0; j < alpha; j++)
     	    tmp = AddMod(tmp, MulMod(tmpZ[j], tmpY[j], p, pinv), p);
     	  if (tmp != 0) {
+	    //	    cout << "failed " << a << " " << b << endl;
     	    delete[] Ypre;
     	    delete[] Zpre;
     	    return -1;  // not generic rank profile
     	  }
+	  //	  cout << "passed " << a << " " << b << endl;
     	  tmpZ += alpha;
     	}
     	tmpY += alpha;
@@ -164,30 +329,35 @@ static long invert_raw(long *Yp, long *Zp, const long alpha,
     tmpYi += alpha;
     tmpZi += alpha;
 
-    for (long i = k+1; i < N; i++){
+    for (long i = k+1; i < (i1-i0); i++){
 
-      long ell = 0, u = 0;
-      for (long j = 0; j < alpha; j++){
+      long ell = 0;
+      for (long j = 0; j < alpha; j++)
 	ell = AddMod(ell, MulModPrecon(tmpYi[j], tmpZk[j], p, Zpre[j]), p);
-        u = AddMod(u, MulModPrecon(tmpZi[j], tmpYk[j], p, Ypre[j]), p);
-      }
-
-
 
       ell = MulModPrecon(ell, rd, p, pd);  // why ell != u?
       ell = MulModPrecon(ell, C.P_r[i+i0], p, C.P_pre[i+i0]);
       ell = MulModPrecon(ell, C.Qab_r[k+j0-i-i0+n-1], p, C.Qab_pre[k+j0-i-i0+n-1]);
 
+      for (long j = 0; j < alpha; j++)
+      	tmpYi[j] = AddMod(tmpYi[j], MulModPrecon(ell, tmpYk[j], p, Ypre[j]), p);
+
+      tmpYi += alpha;
+    }
+
+    for (long i = k+1; i < (j1-j0); i++){
+
+      long u = 0;
+      for (long j = 0; j < alpha; j++)
+        u = AddMod(u, MulModPrecon(tmpZi[j], tmpYk[j], p, Ypre[j]), p);
+
       u = MulModPrecon(u, rd, p, pd); // save one product here ?
       u = MulModPrecon(u, C.P_r[k+i0], p, C.P_pre[k+i0]);
       u = MulModPrecon(u, C.Qab_r[i+j0-k-i0+n-1], p, C.Qab_pre[i+j0-k-i0+n-1]);
 
-      for (long j = 0; j < alpha; j++){
-      	tmpYi[j] = AddMod(tmpYi[j], MulModPrecon(ell, tmpYk[j], p, Ypre[j]), p);
+      for (long j = 0; j < alpha; j++)
       	tmpZi[j] = AddMod(tmpZi[j], MulModPrecon(u, tmpZk[j], p, Zpre[j]), p);
-      }
 
-      tmpYi += alpha;
       tmpZi += alpha;
     }
 
